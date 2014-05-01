b3d24e862151f2934199b55b5e6b266c68f0155c
Ryan Verhey <ryan90v@gmail.com>
2014-05-01 12:47:00 -0500

    Added user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid'].to_i
+      user.name = auth['info']['name']
+      user.username = auth['info']['nickname']
+      user.image_string = auth['info']['image']
+      user.location = auth['info']['location']
+      user.token = auth['credentials']['token']
+      user.secret = auth['credentials']['secret']
+    end
+  end
+end
