1b9f4367ea009062a8d58abc662b3200bfb4409b
Ryan Verhey <ryan90v@gmail.com>
2014-05-01 14:04:25 -0500

    Edited users migration a bit

--- db/migrate/20140501145742_create_users.rb
+++ db/migrate/20140501145742_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :provider
+      t.string  :provider
       t.integer :uid
-      t.string :name
-      t.string :username
-      t.string :image_string
-      t.string :location
-      t.string :token
-      t.string :secret
+      t.string  :name
+      t.string  :username
+      t.string  :image_string
+      t.string  :location
+      t.string  :token
+      t.string  :secret
       t.timestamps
     end
   end
