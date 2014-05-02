7100de53268f58e6b1f66bfab9cb4f3a948f73d1
Ryan Verhey <ryan90v@gmail.com>
2014-05-02 18:27:15 -0500

    Adding more exoplanet info

--- app/assets/javascripts/exoplanetInfo.js
+++ app/assets/javascripts/exoplanetInfo.js
@@ -1,12 +1,60 @@
 var exoPlanets = [
   // Planetarium
-  ["Our Solar System", 41.866333, -87.606783, 7, 'stuff'],
+  ["Our Solar System", 41.866333, -87.606783, 7,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">Our Solar System</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>The Solar System</b> is where we live. It has eight planets, which are Mercury, Venus, Earth (us), '+
+    'Mars, Jupiter, Saturn, Uranus, and Neptune.</p>'+
+    '<p>The marker is at The Adler Planetarium. All of the other extra-solar systems are proportionally distant '+
+    'from here as they are in real life. The scale used is 7 light years to one mile.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    "<li>The Solar System on <a href='http://solarsystem.nasa.gov/planets/'>NASA's Website</li>"+
+    '<li>The Solar System on <a href="http://en.wikipedia.org/wiki/Solar_System">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ],
   // Soldier Field
-  ['Alpha Centauri B', 41.862074, -87.616804, 8, 'stuff'],
+  ['Alpha Centauri A/B', 41.862074, -87.616804, 8,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">Alpha Centauri</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Alpha Centauri</b> is the closest extra-solar star system to our own, being only about 4 light years away. Although it looks like '+
+    'one star to the naked eye, it is actually a binary star system, which means it consists of two stars that orbit '+
+    'each other, Alpha Centauri A and Alpha Centauri B. Alpha Centauri B may also have a rocky terrestrial planet, '+
+    'called Alpha Centauri Bb. Being about 4 light years away, if you could stand on Alpha Centauri Bb right now it would '+
+    'look like the the year '+ ((new Date().getFullYear())-4) +'.</p>'+
+    '<p>The marker is at Soldier Field, which is where the Chicago Bears football team plays. It is about 0.6 miles'+
+    ' from the Adler Planetarium.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    "<li>Our Galactic Neighborhood on <a href='http://interstellar.jpl.nasa.gov/interstellar/probe/introduction/neighborhood.html'>NASA's Website</a></li>"+
+    '<li>Alpha Centauri on <a href="http://en.wikipedia.org/wiki/Alpha_Centauri">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ],
   // Navy Pier Children's Museum
   // ['Tau Ceti e', 41.891105, -87.609497, 3, 'stuff'], Can't do because it's the same system as below
   // Navy Pier Ferris Wheel
-  ['Tau Ceti', 41.891712, -87.607244, 2, 'stuff'],
+  ['Tau Ceti', 41.891712, -87.607244, 2,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">Tau Ceti</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Tau Ceti</b> is a star in the constellation Cetus. It is very similar to our Sun, except it is about '+
+    "75% of the Sun's mass.</p>"+
+    '<p>The marker is at Soldier Field, which is where the Chicago Bears football team plays. It is about 0.6 miles'+
+    ' from the Adler Planetarium.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    "<li>Our Galactic Neighborhood on <a href='http://interstellar.jpl.nasa.gov/interstellar/probe/introduction/neighborhood.html'>NASA's Website</a></li>"+
+    '<li>Alpha Centauri on <a href="http://en.wikipedia.org/wiki/Alpha_Centauri">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ],
   // Sears Tower
   ['GJ 180', 41.878900, -87.635772, 6, 'stuff'],
   // Merch Mart
