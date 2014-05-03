112cb3faa18f3c4b5f6a243f75df4645e2f9a21a
Ryan Verhey <ryan90v@gmail.com>
2014-05-03 15:07:27 -0500

    Completed the information for the map info windows.

--- app/assets/javascripts/exoplanetInfo.js
+++ app/assets/javascripts/exoplanetInfo.js
@@ -58,29 +58,51 @@ var exoPlanets = [
     '</ul>'+
     '</div></div>'
   ],
-  // Sears Tower
-  ['GJ 180', 41.878900, -87.635772, 6,
+  // Millennium Park/Cloud Gate (The Bean)
+  ['Epsilon Eridani', 41.882672, -87.623340, 6,
     '<div id="content">'+
     '<div id="siteNotice"></div>'+
-    '<h1 id="firstHeading" class="firstHeading">GJ 180</h1>'+
+    '<h1 id="firstHeading" class="firstHeading">Epsilon Eridani</h1>'+
     '<div id="bodyContent">'+
-    '<p><b>GJ 180</b> is a star in the constellation Cetus. It is very similar to our Sun, except it is about '+
-    "75% of the Sun's mass. At about 12 light years away, it is one of the closest stars to the Sun. Because it is "+
-    "12 light years away, light that is reaching Tau Ceti right now left the Earth in the year "+ ((new Date().getFullYear())-12) +".</p>"+
-    "<p>Tau Ceti has 5 confirmed planets: Tau Ceti b, c, d, e, and f. Two of those planets, e and f, are within Tau Ceti's "+
-    "habitable zone, which makes them prime targets for the search for extra-terrestrial life.</p>"+
-    "<p>The marker is at Navy Pier's Ferris Wheel. It is about 1.7 miles"+
+    '<p><b>Epsilon Eridani</b> is a star in the constellation Eridanus that is about 10.5 light years away. It is the '+
+    'third closest star to the Sun, and even then light that left Earth in '+((new Date().getFullYear())-11)+' is just getting '+
+    'there now.</p>'+
+    "<p>Epsilon Eridani has 2 planets, Epsilon Eridani b and c. Although Epsilon Eridani b has actually been observed, "+
+    "Epsilon Eridani c hasn't been and its existence is only speculated, because computer simulations of a disty disk that orbit "+
+    "the star show that there could be a second planet in the system.</p>"+
+    "<p>The marker is at Millennium Park, more specifically at the sculpture 'Cloud Gate,' also known as 'The Bean.' It is about 1.5 miles"+
     ' from the Adler Planetarium.</p>'+
     '<p>Resources:</p>'+
     '<ul>'+
-    "<li>Tau Ceti on <a href='http://www.princeton.edu/~achaney/tmve/wiki100k/docs/Tau_Ceti.html'>Princeton University's Website</a></li>"+
-    '<li>Tau Ceti on <a href="http://en.wikipedia.org/wiki/Tau_Ceti">Wikipedia</a></li>'+
+    "<li>Epsilon Eridani b on <a href='http://exoplanet.eu/catalog/eps_eridani_b/'>Exoplanet.eu</a></li>"+
+    "<li>Epsilon Eridani on <a href='http://apod.nasa.gov/apod/ap000807.html'>NASA's Astronomy Picture of the Day</a></li>"+
+    '<li>Epsilon Eridani on <a href="http://en.wikipedia.org/wiki/Epsilon_Eridani">Wikipedia</a></li>'+
     '</ul>'+
     '</div></div>'
   ],
   // Merch Mart
-  ['Gliese 876', 41.888477, -87.635407, 5, 'stuff'],
-  // This is the one that's at DBC
+  ['Gliese 876', 41.888477, -87.635407, 5,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">Gliese 876</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Gliese 876</b> is a red dwarf star in the constellation Aquarius and is about 15 light years away. So light that left '+
+    'Earth in '+((new Date().getFullYear())-15)+' is just getting to Gliese 876 now. As a red dwarf, Gliese 876 is much smaller and much '+
+    'less hot than the Sun, and it is only about 1.25% as bright as the Sun.</p>'+
+    "<p>Gliese 876 is the closest star to have a confirmed multi-planetary system. It has 4 planets, Gliese 876 d, c, b, and e. "+
+    "The inner-most planet, Gliese 876 d, is likely a rocky planet, while the rest are speculated to be gas giants. Gliese 876 b "+
+    "happens to be the first planet ever discovered orbiting a red dwarf star.</p>"+
+    "<p>This marker is at the Merchandise Mart, which is about 2.14 miles"+
+    ' from the Adler Planetarium.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    "<li>Gliese 876 d on <a href='http://exoplanet.eu/catalog/gliese_876_d/'>Exoplanet.eu</a></li>"+
+    "<li>Gliese 876 on <a href='http://apod.nasa.gov/apod/ap980626.html'>NASA's Astronomy Picture of the Day</a></li>"+
+    '<li>Gliese 876 on <a href="http://en.wikipedia.org/wiki/Gliese_876">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ],
+  // DBC
   ["Gliese 682",41.889911, -87.637657, 4,
     '<div id="content">'+
     '<div id="siteNotice"></div>'+
@@ -97,9 +119,63 @@ var exoPlanets = [
     '</div></div>'
   ],
   // US Cell Field
-  ['82 G. Eridani', 41.830273, -87.633348, 9, 'stuff'],
+  ['82 G. Eridani', 41.830273, -87.633348, 9,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">82 G. Eridani</h1>'+
+    '<div id="bodyContent">'+
+    "<p><b>82 G. Eridani</b> is a main sequence star, like the Sun, that's a little under 20 light years away from Earth in the constellation Eridanus. That "+
+    "means that light that left Earth in the year "+ ((new Date().getFullYear())-20) +" is just reaching 82 G. Eridani now.</p>"+
+    "<p>82 G. Eridani has three planets, 82 G. Eridani b, c, and d. They are all classified as 'super-Earths,' which means "+
+    "they have just a few times the mass of Earth. Each one also orbit 82 G. Eridani in 90 days or less, meaning "+
+    "they are very close to the star.</p>"+
+    "<p>This marker is at US Cellular Field, the home of the Chicago White Sox, which is about 2.82 miles"+
+    ' from the Adler Planetarium.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    '<li>82 G. Eridani on <a href="http://en.wikipedia.org/wiki/82_G._Eridani">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ],
   // Wrigley
-  ['HD 40307', 41.947854, -87.655642, 1, 'stuff'],
-  // Museum of Science and Industry
-  ['HD 85512', 41.790573, -87.583066, 10, 'stuff']
+  ['HD 40307', 41.947854, -87.655642, 1,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">HD 40307</h1>'+
+    '<div id="bodyContent">'+
+    "<p><b>HD 40307</b> is a star in the constellation Pictor that is about 42 light years away. That means that "+
+    "light that left Earth in the year "+ ((new Date().getFullYear())-42) +" is just reaching HD 40307 now.</p>"+
+    "<p>HD 40307 has an astonishing 6 planets, 5 of which orbit closer than Mercury orbits our Sun. The last planet "+
+    "orbits about as far away from HD 40307 as Venus orbits the Sun, and lies within HD 40307's habitable zone.</p>"+
+    "<p>This marker is at Wrigley Field, the home of the Chicago Cubs, which is about 6 miles"+
+    ' from the Adler Planetarium.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    '<li>HD 40307 on <a href="http://exoplanet.eu/catalog/?f=%27HD%2040307%27+in+name">Exoplanet.eu</a></li>'+
+    '<li>HD 40307 on <a href="http://en.wikipedia.org/wiki/HD_40307">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ],
+  // Site of the first sustained Nuclear reaction, old Stagg Field , university of chicago Chicago Pile 1
+  ['HD 85512', 41.792015, -87.599959, 10,
+    '<div id="content">'+
+    '<div id="siteNotice"></div>'+
+    '<h1 id="firstHeading" class="firstHeading">HD 85512</h1>'+
+    '<div id="bodyContent">'+
+    "<p><b>HD 85512</b> is a star that's about 36 light years away from Earth in the constellation Vela. Because it is "+
+    "36 light years away, That means that light that left Earth in the year "+ ((new Date().getFullYear())-36) +" is just "+
+    "reaching HD 85512 now.</p>"+
+    "<p>HD 85512 has one planet, HD 85512 b, that is about 3 times the mass of Earth. It also lies within "+
+    "HD 85512's habitable zone. Scientists believe has the potential to be cool enough to have liquid water "+
+    "on its surface, making it potentially habitable.</p>"+
+    "<p>This marker is at the University of Chicago's Regenstein Library, which is on the site of the first "+
+    "sustained nuclear reaction on Earth. It is about 5.15 miles"+
+    ' from the Adler Planetarium.</p>'+
+    '<p>Resources:</p>'+
+    '<ul>'+
+    '<li>HD 85512 b on <a href="http://exoplanet.eu/catalog/hd_85512_b/">Exoplanet.eu</a></li>'+
+    '<li>HD 85512 on <a href="http://en.wikipedia.org/wiki/HD_85512">Wikipedia</a></li>'+
+    '</ul>'+
+    '</div></div>'
+  ]
 ]
