eac302c5601c3dbf10ae641aca0507d2e2d41557
Ryan Verhey <ryan90v@gmail.com>
2014-05-03 16:09:00 -0500

    Locations were being seeded twice. Also added updated system information

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,11 @@ exosystems = [
   # // Planetarium
   ["Our Solar System", 'Adler Planetarium', 41.866333, -87.606783],
   # // Soldier Field
-  ['Alpha Centauri B', 'Soldier Field', 41.862074, -87.616804],
-  # // Navy Pier Children's Museum
-  # ['Tau Ceti e', "Navy Pier Children's Museum", 41.891105, -87.609497], Can't do because same system as below
+  ['Alpha Centauri A/B', 'Soldier Field', 41.862074, -87.616804],
   # // Navy Pier Ferris Wheel
   ['Tau Ceti', "Navy Pier Ferris Wheel", 41.891712, -87.607244],
   # // Sears Tower
-  ['GJ 180', "Willis Tower", 41.878900, -87.635772],
+  ['Epsilon Eridani', "Millennium Park", 41.882672, -87.623340],
   # // Merch Mart
   ['Gliese 876', 'Merchandise Mart', 41.888477, -87.635407],
   # // This is the one that's at DBC
@@ -20,9 +18,10 @@ exosystems = [
   # // Wrigley
   ['HD 40307', "Wrigley Field", 41.947854, -87.655642],
   # // Museum of Science and Industry
-  ['HD 85512', "The Museum of Science and Industry", 41.790573, -87.583066]
+  ['HD 85512', "Chicago Pile-1", 41.792015, -87.599959]
 ]
 
+# Creating Locations and Systems
 exosystems.each do |x|
   loc = Location.create!(name: x[1], latitude: x[2], longitude: x[3])
   system = System.create!(name: x[0])
@@ -36,29 +35,6 @@ end
 
 users = [1, 2, 3, 4, 5]
 locations = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-#Create Locations
-Location.create(name: 'Adler Planetarium', latitude: 41.866333, longitude: -87.606783)
-Location.create(name: 'Soldier Field', latitude: 41.862074, longitude: -87.616804)
-Location.create(name: 'Nay Pier Ferris Wheel', latitude: 41.891712, longitude: -87.607244)
-Location.create(name: 'Willis Tower', latitude: 41.878900, longitude: -87.635772)
-Location.create(name: 'Merchandise Mart', latitude: 41.888477, longitude: -87.635407)
-Location.create(name: 'Dev Bootcamp', latitude: 41.889911, longitude: -87.637657)
-Location.create(name: 'US Cellular Field', latitude: 41.830273, longitude: -87.633348)
-Location.create(name: 'Wrigley Field', latitude: 41.947854, longitude: -87.655642)
-Location.create(name: 'Museum of Science and Industry', latitude: 41.790573, longitude: -87.583066)
-
-#Create Systems
-System.create(name: "Our Solar System" , location_id: 1)
-System.create(name: "Alpha Centauri Bb", location_id: 2)
-System.create(name: "Tau Ceti", location_id: 3)
-System.create(name: "GJ 180" , location_id: 4)
-System.create(name: "Gliese 876" , location_id: 5)
-System.create(name: "Gliese 682", location_id: 6)
-System.create(name: '82 G. Eridani', location_id: 7)
-System.create(name: 'HD 40307', location_id: 8)
-System.create(name: 'HD 85512', location_id: 9)
 
 #Create Visits
 Visit.create(user_id: 1, location_id: 1)
