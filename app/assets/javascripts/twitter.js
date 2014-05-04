3fee6c8b99c566c7e1d4f313418010f0dbee9929
Ryan Verhey <ryan90v@gmail.com>
2014-05-03 22:25:37 -0500

    Made it so if the twitter feed is activated and you click anywhere else, it disappears

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -3,4 +3,15 @@ $(function() {
     e.preventDefault();
     $("#twitter-popup").fadeToggle(150);
   });
+  $(document).click(function(e) {
+    if ($(e.target).is(".hashtag a")) {
+      e.preventDefault();
+      console.log(".hashtag")
+    }
+    var target = e.target;
+    if (!$(target).is("#twitter-popup") && !$(target).is('.hashtag a')) {
+      console.log("popup")
+      $("#twitter-popup").fadeOut(150);
+    };
+  });
 });
