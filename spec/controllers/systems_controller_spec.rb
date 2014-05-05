be4da36139d915762d9e9c507e330396d5639ed6
Michael Main <michaelanthonymain@gmail.com>
2014-05-05 15:37:46 -0500

    Fixed the controller specs, 56 examples, 0 failures.

--- spec/controllers/systems_controller_spec.rb
+++ spec/controllers/systems_controller_spec.rb
@@ -10,4 +10,19 @@ describe SystemsController do
           expect(assigns(:system)).to eq(system)
       end
     end
+
+    describe "GET #all" do
+        let!(:location) { FactoryGirl.create(:location) }
+        let!(:system) { FactoryGirl.create(:system, location_id: location.id) }
+
+    it "should assign @systems to each location's system" do
+        get :all
+        expect(assigns(:systems)).to eq( {location.id => location.system} )
+    end
+
+    it "should render json" do
+        get :all
+            response.body.should == {location.id => location.system}.to_json
+        end
+    end
 end
