5d00f53cd0721c410c2e4832fea6e08bbddbda28
Ryan Verhey <ryan90v@gmail.com>
2014-05-05 15:45:04 -0500

    New and improved photo uploading! No longer saves hex data in DB. now saved in the public/collections directory

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,14 +3,19 @@ class CollectionsController < ApplicationController
 
   def create
     # Still need to add user_id and location_id, but it works for a start
-    @c = Collection.new(text:params[:text], image: params[:image])
+    name = SecureRandom.hex + ".png"
+    dir = "collections"
+    unpack = params[:image].unpack("m0")
+    path = File.join("public",dir, name)
+    File.open(path, "wb") do |f|
+      f.write(unpack.first)
+    end
+    @c = Collection.new(text: params[:text], image: name)
     @c.save!
   end
 
   def show
-    @c = []
-    @c << Collection.last
-    @c << Collection.find(Collection.last.id - 1)
+    @c = Collection.find(params[:id])
     render :create
   end
 end
