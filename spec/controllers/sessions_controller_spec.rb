0f2bc9acab4e86b11245bb2dec7d77e37c14c53c
Michael Main <michaelanthonymain@gmail.com>
2014-05-05 09:39:29 -0500

    Tests for the sessions controller.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe SessionsController do
+   before do
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+    end
+
+  context "#create" do
+    it "should successfull create a user" do
+      expect {
+        post :create, provider: :twitter
+      }.to change{ User.count }.by(1)
+    end
+
+    it "should successfully create a session" do
+      session[:user_id].should be_nil
+      post :create, provider: :twitter
+      session[:user_id].should_not be_nil
+    end
+
+    it "should redirect to the root path" do
+      post :create, provider: :twitter
+      response.should redirect_to root_path
+    end
+  end
+
+  context "#destroy" do
+    before do
+      post :create, provider: :twitter
+    end
+
+    it "should clear the session" do
+      session[:user_id].should_not be_nil
+      delete :destroy
+      session[:user_id].should be_nil
+    end
+
+    it "should redirect to the home page" do
+      delete :destroy
+      response.should redirect_to root_path
+    end
+  end
 
 end
