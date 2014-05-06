ee4d7d7fbc5564dc9811423f2cec77c525fd63df
Michael Main <michaelanthonymain@gmail.com>
2014-05-05 21:03:09 -0500

    completed user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,4 +35,27 @@ describe UsersController do
          response.body.should == { "#{location.id}" => 'Test Location'}.to_json
      end
   end
+
+  context "#mobile" do
+      let(:location) { FactoryGirl.create(:location) }
+
+      it "should find the user by params uid" do
+        post :mobile, {user_id: user.uid}
+        expect(assigns(:user)).to eq(user)
+      end
+
+      it "should create a user if none already exists" do
+        expect {
+        post :mobile, {user_id: 14}
+        }.to change{ User.count }.by(1)
+      end
+
+      it "should add a new location to the user's locations" do
+        locations = user.locations
+        expect {
+          post :mobile, {user_id: user.uid, location_id: location.id }
+        }.to change{ locations.count }.by(1)
+      end
+  end
+
 end
