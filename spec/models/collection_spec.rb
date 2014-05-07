458e392f5b2f6cc3ccaf17ebc24078077eeba038
Michael Main <michaelanthonymain@gmail.com>
2014-05-07 16:46:42 -0500

    Fixed spec

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -5,7 +5,10 @@ describe Collection do
   it { should have_db_column(:text).of_type(:text) }
   it { should have_db_column(:location_id).of_type(:integer) }
   it { should have_db_column(:user_id).of_type(:integer) }
-  it { should have_db_column(:image).of_type(:text) }
+  it { should have_db_column(:image_file_name).of_type(:string) }
+  it { should have_db_column(:image_content_type).of_type(:string) }
+  it { should have_db_column(:image_file_size).of_type(:integer) }
+  it { should have_db_column(:image_updated_at).of_type(:datetime) }
 
   #Association Tests
   it { should belong_to(:user) }
